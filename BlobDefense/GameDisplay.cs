fc46ee0919b29adb7316b17c4b4c3e0113347539
Mathias <mathiassoeholm@gmail.com>
2012-12-06 19:53:19 +0100

    Added summary header to GameDisplay

--- BlobDefense/GameDisplay.cs
+++ BlobDefense/GameDisplay.cs
@@ -10,11 +10,14 @@ using System.Windows.Forms;
 
 namespace BlobDefense
 {
+    /// <summary>
+    /// The windows form which is responsible of displaying the game.
+    /// </summary>
     public partial class GameDisplay : Form
     {
         public GameDisplay()
         {
-            InitializeComponent();
+            this.InitializeComponent();
         }
     }
 }
