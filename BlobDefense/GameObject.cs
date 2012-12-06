77fd2c5231664e4021c83f27524fff38d7ebf47e
Mathias <mathiassoeholm@gmail.com>
2012-12-06 19:48:44 +0100

    Made base class for all game objects

---
+++ BlobDefense/GameObject.cs
@@ -0,0 +1,40 @@
+﻿namespace BlobDefense
+{
+    using System.Collections.Generic;
+    using System.Drawing;
+
+    /// <summary>
+    /// Base class for all entities in the game.
+    /// </summary>
+    internal abstract class GameObject
+    {
+        protected GameObject()
+        {
+            // Initialize a new list if it is null
+            AllGameObjects = AllGameObjects ?? new List<GameObject>();
+
+            // Add this game object to the game objects list
+            AllGameObjects.Add(this);
+        }
+
+        /// <summary>
+        /// Finalizes an instance of the <see cref="GameObject"/> class,
+        /// removing it from the game object list, when destructed.
+        /// </summary>
+        ~GameObject()
+        {
+            // Remove this game object from the game objects list
+            AllGameObjects.Remove(this);
+        }
+
+        /// <summary>
+        /// Gets a list of all the game objects created.
+        /// </summary>
+        public static List<GameObject> AllGameObjects { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the position of the game object in pixel coordinates.
+        /// </summary>
+        public PointF Position { get; protected set; }
+    }
+}
