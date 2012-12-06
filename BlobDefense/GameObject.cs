81e467c173f7b26cff4b963631a837226238a97a
Mathias <mathiassoeholm@gmail.com>
2012-12-06 20:13:30 +0100

    Added render logic for game objects

--- BlobDefense/GameObject.cs
+++ BlobDefense/GameObject.cs
@@ -40,5 +40,31 @@
         /// Gets or sets the position of the game object in pixel coordinates.
         /// </summary>
         public PointF Position { get; protected set; }
+
+        /// <summary>
+        /// Gets or sets the source rectangle used on the sprite sheet.
+        /// </summary>
+        protected RectangleF SpriteSheetSource { get; set; }
+
+        /// <summary>
+        /// Gets or sets the sprite sheet for this game object.
+        /// </summary>
+        protected Image SpriteSheet { get; set; }
+
+        /// <summary>
+        /// Displays the game object in the graphics context using pixel coordinates.
+        /// </summary>
+        /// <param name="context">
+        /// The graphics context used to display the game object.
+        /// </param>
+        public void Render(Graphics context)
+        {
+            context.DrawImage(
+                image: this.SpriteSheet,
+                x: (int)this.Position.X,
+                y: (int)this.Position.Y,
+                srcRect: this.SpriteSheetSource,
+                srcUnit: GraphicsUnit.Pixel);
+        }
     }
 }
