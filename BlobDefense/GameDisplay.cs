2e5c6236efffb251e86ae938ad265113cd6c0bee
Mathias <mathiassoeholm@gmail.com>
2012-12-06 19:55:59 +0100

    Cleaned the GameDisplay up a bit

--- BlobDefense/GameDisplay.cs
+++ BlobDefense/GameDisplay.cs
@@ -1,15 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace BlobDefense
+﻿namespace BlobDefense
 {
+    using System.Windows.Forms;
+    
     /// <summary>
     /// The windows form which is responsible of displaying the game.
     /// </summary>
