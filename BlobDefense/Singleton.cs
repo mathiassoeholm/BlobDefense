b85f8d97f1db6d2607bbf65bfe4b6b17367148d1
Mathias <mathiassoeholm@gmail.com>
2012-12-12 22:38:44 +0100

    Made generic singleton class

---
+++ BlobDefense/Singleton.cs
@@ -0,0 +1,16 @@
+﻿namespace BlobDefense
+{
+    using System;
+
+    public class Singleton<T> where T : class, new()
+    {
+        protected Singleton()
+        {
+        }
+
+        private static readonly Lazy<T> lazy =
+            new Lazy<T>(() => new T());
+
+        public static T Instance { get { return lazy.Value; } }
+    }
+}
