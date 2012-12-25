1a16e0b44f8b788e42a6fb0208ac5093a1606729
Mathias <mathiassoeholm@gmail.com>
2012-12-25 19:52:13 +0100

    Added enemy class, and implemented hit detection

---
+++ BlobDefense/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlobDefense
+{
+    internal class Enemy : MovingGameObject
+    {
+        private int targetNode;
+        
+        protected override void OnTargetHit()
+        {
+            // TODO: Select new target in the path list, and set targetNode
+            throw new NotImplementedException();
+        }
+    }
+}
--- BlobDefense/MovingGameObject.cs
+++ BlobDefense/MovingGameObject.cs
@@ -9,22 +9,48 @@ namespace BlobDefense
 {
     using System.Drawing;
 
-    internal class MovingGameObject : GameObject
+    internal abstract class MovingGameObject : GameObject
     {
+        /// <summary>
+        /// The current target this game object is moving towards.
+        /// </summary>
         private PointF currentTarget;
 
-        protected float moveSpeed { get; set; }
+        /// <summary>
+        /// Gets or sets the move speed.
+        /// </summary>
+        protected float MoveSpeed { get; set; }
 
+        /// Moves the game object towards the current target.
+        /// </summary>
         public void Move()
         {
             // Get the move direction
-            var moveDirection = new PointF(this.currentTarget.X - this.Position.X, currentTarget.Y - this.Position.Y);
+            var moveDirection = new PointF(this.currentTarget.X - this.Position.X, this.currentTarget.Y - this.Position.Y);
 
             // Set length of the move direction based on the move speed
-            moveDirection.SetMagnitude(this.moveSpeed * Time.DeltaTime);
+            moveDirection.SetMagnitude(this.MoveSpeed * Time.DeltaTime);
             
+            // Check if we pass the target in any axis
+            if ((this.Position.X > this.currentTarget.X && this.Position.X + moveDirection.X < this.currentTarget.X)
+                || (this.Position.X < this.currentTarget.X && this.Position.X + moveDirection.X > this.currentTarget.X)
+                || (this.Position.Y < this.currentTarget.Y && this.Position.Y + moveDirection.Y > this.currentTarget.Y)
+                || (this.Position.Y > this.currentTarget.Y && this.Position.Y + moveDirection.Y > this.currentTarget.Y))
+            {
+                
+                // Set position to target
+                this.Position = this.currentTarget;
+
+                // Perform on hit action
+                this.OnTargetHit();
+
+                return;
+            }
+
             // Add the movement to our position
             this.Position.Add(moveDirection);
         }
+
+        protected abstract void OnTargetHit();
     }
 }
