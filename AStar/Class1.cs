6f7390f0570d786bd153e67dd859eb944be0f899
Mathias <mathiassoeholm@gmail.com>
2012-12-25 21:36:36 +0100

    Added A* project to the solution

---
+++ AStar/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AStar
+{
+    using System.Drawing;
+
+    public static class Astar
+    {
+        public static List<PointF> GetPath()
+        {
+            
+        }
+    }
+}
